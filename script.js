0f4845b9c199ffb2edfe003daaa34f590649a38d
KYOOOZA <darrensandyaputra@gmail.com>
2026-09-14 14:45:35 +0700

    Update website

--- script.js
+++ script.js
@@ -58,12 +58,13 @@ const products = [
     { id: 40, name: "Pasir Cor", category: "Semen & Bahan Bangunan", image: "semen/Pasir Cor.jpeg", description: "Pasir Cor untuk pekerjaan konstruksi." },
     { id: 41, name: "Kerikil", category: "Semen & Bahan Bangunan", image: "semen/Kerikil.jpg", description: "Kerikil untuk campuran beton." },
     { id: 42, name: "Batu Bata", category: "Semen & Bahan Bangunan", image: "semen/Batu Bata.jpeg", description: "Batu bata untuk dinding bangunan." },
+    { id: 43, name: "Gypsum", category: "Semen & Bahan Bangunan", image: "semen/Gypsum.jpeg", description: "Gypsum untuk dinding bangunan." },
 
     // PERKAKAS, BAUT & AKSESORIS
-    { id: 43, name: "Paku Bangunan", category: "Perkakas, Baut & Aksesoris", image: "perkakas/Paku Bangunan.jpg", description: "Paku untuk berbagai kebutuhan bangunan." },
-    { id: 44, name: "Baut Besi", category: "Perkakas, Baut & Aksesoris", image: "perkakas/Baut Besi.jpg", description: "Baut untuk kebutuhan konstruksi." },
-    { id: 45, name: "Obeng Set", category: "Perkakas, Baut & Aksesoris", image: "perkakas/Obeng Set.jpg", description: "Set obeng untuk pekerjaan rumah." },
-    { id: 46, name: "Tang Kombinasi", category: "Perkakas, Baut & Aksesoris", image: "perkakas/Tang Kombinasi.jpg", description: "Tang kombinasi untuk berbagai pekerjaan." }
+    { id: 44, name: "Paku Bangunan", category: "Perkakas, Baut & Aksesoris", image: "perkakas/Paku Bangunan.jpg", description: "Paku untuk berbagai kebutuhan bangunan." },
+    { id: 45, name: "Baut Besi", category: "Perkakas, Baut & Aksesoris", image: "perkakas/Baut Besi.jpg", description: "Baut untuk kebutuhan konstruksi." },
+    { id: 46, name: "Obeng Set", category: "Perkakas, Baut & Aksesoris", image: "perkakas/Obeng Set.jpg", description: "Set obeng untuk pekerjaan rumah." },
+    { id: 47, name: "Tang Kombinasi", category: "Perkakas, Baut & Aksesoris", image: "perkakas/Tang Kombinasi.jpg", description: "Tang kombinasi untuk berbagai pekerjaan." }
 ];
 
 const ADMIN_WA = "6285376765758";
